270057c4fa4c59d23e50bb83d293bbe64f2b1073
alvarosaavedrau <cloud@alvarosaavedra.es>
2025-08-14 12:20:47 +0200

    Movidos recursos a fichero project.tf

--- main.tf
+++ main.tf
@@ -14,33 +14,3 @@ terraform {
     use_lockfile = true
   }
 }
-
-resource "vercel_project" "vercel_project" {
-  for_each = var.vercel_projects
-
-  name          = replace(each.key, ".", "-")
-  framework     = each.value.framework
-  build_command = each.value.build_command
-  node_version  = each.value.node_version
-  vercel_authentication = {
-    deployment_type = "standard_protection"
-  }
-
-  git_repository = {
-    type              = each.value.git_type
-    repo              = "${var.github_username}/${each.key}"
-    production_branch = "main"
-  }
-
-  environment = each.value.environment_variables
-}
-
-resource "vercel_project_domain" "vercel_project_domain" {
-  for_each = {
-    for k, v in var.vercel_projects : k => v
-    if lookup(v, "custom_domain", false)
-  }
-
-  project_id = vercel_project.vercel_project[each.key].id
-  domain     = each.key
-}
